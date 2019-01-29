e4a89cf2c2433aef27264e327f97eeefbc786526
Lev Romanenko <romanenko.lew@gmail.com>
2019-01-29 20:33:53 +0200

    There was drawn xml weather forecast

--- app/src/main/java/com/romanenko/lew/progforcetask/mapScreen/MapFragment.java
+++ app/src/main/java/com/romanenko/lew/progforcetask/mapScreen/MapFragment.java
@@ -5,11 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,9 +24,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.romanenko.lew.progforcetask.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MapFragment extends Fragment {
 
     SupportMapFragment mMapFragment;
+
+    @BindView(R.id.weather_forecast)
+    Button mWeatherForecast;
 
     public static MapFragment MapFragment() {
         // Required empty public constructor
@@ -40,35 +49,41 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.map_fragment, container, false);
+        View view = inflater.inflate(R.layout.map_fragment, container, false);
+        ButterKnife.bind(this, view);
         mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);
         onMapReady();
-        return rootView;
+        return view;
     }
 
-     private void onMapReady(){
-         mMapFragment.getMapAsync(new OnMapReadyCallback() {
-             @Override
-             public void onMapReady(GoogleMap mMap) {
-                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    private void onMapReady() {
+        mMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-               //  mMap.clear(); //clear old markers
+                //  mMap.clear(); //clear old markers
 
-                 CameraPosition googlePlex = CameraPosition.builder()
-                         .target(new LatLng(37.4219999,-122.0862462))
-                         .zoom(10)
-                         .bearing(0)
-                         .tilt(45)
-                         .build();
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(37.4219999, -122.0862462))
+                        .zoom(10)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
 
-                 mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
 
-                 mMap.addMarker(new MarkerOptions()
-                         .position(new LatLng(37.4219999, -122.0862462))
-                         .title("Spider Man"));
-                 // .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.spider)));
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4219999, -122.0862462))
+                        .title("Spider Man"));
+                // .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.spider)));
 
-             }
-         });
-     }
+            }
+        });
+    }
+
+    @OnClick(R.id.weather_forecast)
+    public void onClickWeatherForecast() {
+
+    }
 }
--- app/src/main/java/com/romanenko/lew/progforcetask/weatherScreen/WeatherFragment.java
+++ app/src/main/java/com/romanenko/lew/progforcetask/weatherScreen/WeatherFragment.java
@@ -1,4 +1,28 @@
 package com.romanenko.lew.progforcetask.weatherScreen;
 
-public class WeatherFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.romanenko.lew.progforcetask.R;
+import com.romanenko.lew.progforcetask.mapScreen.MapFragment;
+
+public class WeatherFragment extends Fragment {
+
+    public static WeatherFragment MapFragment() {
+        return new WeatherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.map_fragment, container, false);
+
+
+        return rootView;
+    }
 }
